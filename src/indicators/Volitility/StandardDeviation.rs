7a116e60f0ed721450472257efb06ffc2319b6e6
B-litljon <brandonc.littlejohn@gmail.com>
2024-11-18 16:03:17 -0800

    add two new volitility indicators, standard deviation and variance.

--- src/indicators/Volitility/StandardDeviation.rs
+++ src/indicators/Volitility/StandardDeviation.rs
@@ -1,1 +1,14 @@
+mod momentum;
+mod variance;
 use std::collections::VecDeque;
+
+pub fn standard_deviation(data: &VecDeque<f64>, period: usize) -> Option<f64> {
+    if data.len() < period {
+        return None;
+    }
+    let mut ma = momentum::MovingAvg::new(period);
+    let mean = ma.simple(*data.back().unwrap());
+    let variance = variance::variance(data, period, mean);
+
+    Some(variance.sqrt())
+}
---
+++ src/indicators/Volitility/Variance.rs
@@ -0,0 +1,10 @@
+use std::collections::VecDeque;
+
+pub fn variance(data: &VecDeque<f64>, period: usize, mean: f64) -> f64 {
+    data.iter()
+        .rev()
+        .take(period)
+        .map(|value| (value - mean).powi(2))
+        .sum::<f64>()
+        / period as f64
+}
